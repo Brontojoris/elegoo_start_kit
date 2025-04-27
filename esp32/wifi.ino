12fc6dcedbbc7962921a0f53a8c4db34df2ac946
Joris de Beer <joris.debeer@maxisdigital.com.au>
2025-04-27 13:11:20 +1000

    Notes for connecting ESP32 to WiFi

--- esp32/wifi.ino
+++ esp32/wifi.ino
@@ -7,13 +7,13 @@
 ```
 WiFi.mode(WIFI_STA)
 ```
-In this mode, ESP32 connects to the other network
-such as a Wi-Fi router. In the connection process,
-the Wi-Fi router assigns the unique IP address to
-the ESP32.
+In this mode, ESP32 connects to the other network 
+such as a Wi-Fi router. In the connection process, 
+the Wi-Fi router assigns the unique IP address to 
+the ESP32. 
 
 The router is connected to the internet, so we can
-request data from the internet i.e. Cloud, APIs,
+request data from the internet i.e. Cloud, APIs, 
 also we can send the data to the internet.
 
 # Mode 2: ESP32 Access Point
@@ -26,9 +26,9 @@ In Access point mode, ESP32 creates its own Wi-Fi
 network. It acts as a router and other devices can
 connect to the ESP32 like smartphones and laptops.
 
-This mode is mentioned as soft-AP (Soft Access Point).
+This mode is mentioned as soft-AP (Soft Access Point). 
 
-# Mode 3: ESP32 Access point and Hotspot Mode
+# Mode 3: ESP32 Access point and Hotspot Mode 
 
 ```
 WiFi.mode(WIFI_AP_STA)
@@ -60,27 +60,27 @@ const char* ssid = "<WiFi SSID>";
 const char* password = "<wifi-password>>";
 
 void initWiFi() {
-    WiFi.mode(WIFI_STA);    //Set Wi-Fi Mode as station
-    WiFi.begin(ssid, password);
-
-    Serial.println("Connecting to WiFi ..");
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print('.');
-        delay(1000);
-    }
-
-    Serial.println(WiFi.localIP());
-    Serial.print("RRSI: ");
-    Serial.println(WiFi.RSSI());
+ WiFi.mode(WIFI_STA);    //Set Wi-Fi Mode as station
+ WiFi.begin(ssid, password);   
+ 
+ Serial.println("Connecting to WiFi ..");
+ while (WiFi.status() != WL_CONNECTED) {
+   Serial.print('.');
+   delay(1000);
+ }
+ 
+ Serial.println(WiFi.localIP());
+ Serial.print("RRSI: ");
+ Serial.println(WiFi.RSSI());
 }
 
 void setup() {
-    Serial.begin(115200);
-    initWiFi();
+ Serial.begin(115200);
+ initWiFi();
 }
 
 void loop() {
-    // Log some info to console
-    Serial.println(WiFi.RSSI());
-    delay(2000);
+  // Log some info to console
+  Serial.println(WiFi.RSSI());
+  delay(2000);
 }
